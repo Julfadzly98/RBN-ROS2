18b87f76f1abd14c523608e93be899d86c79d357
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-23 15:21:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template
-import serial
+import requests
 
 app = Flask(__name__)
+
+ESP32_IP = "ESP32_IP_ADDRESS"  # Replace with the actual IP address of your ESP32
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/<action>')
+@app.route('/control/<action>')
 def control(action):
-    if action == "point_a":
-        ser.write(b'1')
-    elif action == "point_b":
-        ser.write(b'2')
+    url = f"http://{ESP32_IP}/{action}"
+    response = requests.get(url)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyS0', baudrate=115200)  # Assuming ESP32 is connected to UART0
     app.run(debug=True, host='0.0.0.0')
