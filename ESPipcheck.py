5a20d3001219fb2514583440295356d14da5d6cc
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-23 15:40:21 +0800

    Update ESPipcheck.py

--- ESPipcheck.py
+++ ESPipcheck.py
@@ -1,4 +1,25 @@
 import network
 
+def connect():
+    import network
+ 
+    ssid = "smartspacekk" 
+    password = "smartspace09" 
+ 
+    station = network.WLAN(network.STA_IF)
+ 
+    if station.isconnected() == True:
+        print("Already connected")
+        return
+ 
+    station.active(True)
+    station.connect(ssid, password)
+ 
+    while station.isconnected() == False:
+        pass
+ 
+    print("Connection successful")
+    print(station.ifconfig())
+
 wlan = network.WLAN(network.STA_IF)
 print(wlan.ifconfig())
