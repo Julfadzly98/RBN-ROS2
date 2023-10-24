8acfb65fe8b7a567e5f076774f632e79d30ae201
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 14:04:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-# Install Flask if not already installed: pip install Flask
-from flask import Flask, request
-import requests
+import serial
+import time
 
-app = Flask(__name__)
+# Set up the serial connection
+ser = serial.Serial('/dev/serial0', 115200, timeout=1)  # Change the port if necessary
 
-@app.route('/')
-def index():
-    return '''
-    <form action="/send_signal" method="post">
-        <input type="submit" value="Send Signal">
-    </form>
-    '''
+def send_to_esp32(message):
+    ser.write(message.encode('utf-8'))
 
-@app.route('/send_signal', methods=['POST'])
-def send_signal():
-    # Send a request to ESP32
-    response = requests.get('http://esp32_ip_address:port/receive_signal')
-    return response.text
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# Example usage:
+send_to_esp32('Hello from Raspberry Pi!')
