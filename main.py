47a6bb325d9033b1159553d07ade2664c74ff5eb
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 12:24:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-from flask import Flask, render_template
+# Import necessary modules
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
-ESP32_IP = "ESP32_IP_ADDRESS"  # Replace with the actual IP address of your ESP32
+# Define the HTML template with two buttons
+html = '''
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Button Control</title>
+  </head>
+  <body>
+    <form action="/send_signal" method="post">
+      <input type="submit" name="button" value="Button 1">
+      <input type="submit" name="button" value="Button 2">
+    </form>
+  </body>
+</html>
+'''
 
+# Route to render the HTML template
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return html
 
-@app.route('/control/<action>')
-def control(action):
-    url = f"http://{ESP32_IP}/{action}"
-    response = requests.get(url)
-    return render_template('index.html')
+# Route to send a signal to the ESP32
+@app.route('/send_signal', methods=['POST'])
+def send_signal():
+    button_pressed = request.form['button']
+    if button_pressed == 'Button 1':
+        requests.get('http://ESP32_IP_ADDRESS/signal?button=1')
+    elif button_pressed == 'Button 2':
+        requests.get('http://ESP32_IP_ADDRESS/signal?button=2')
+    return "Signal Sent!"
 
+# Run the application
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
