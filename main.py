4c530560bbdad40fddf04d77c29c565827757eb0
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 16:18:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import serial
 import time
+from flask import Flask, render_template
 
 ser = serial.Serial('/dev/serial0', 9600)  # Open the serial port
+
+app = Flask(__name__)
 
 def send_to_esp32(message):
     try:
@@ -10,5 +13,19 @@ def send_to_esp32(message):
     except Exception as e:
         print(f"Error: {e}")
 
-# Example usage:
-send_to_esp32('Hello from Raspberry Pi!')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/button1')
+def button1():
+    send_to_esp32('Button 1 pressed!')
+    return 'Button 1 pressed!'
+
+@app.route('/button2')
+def button2():
+    send_to_esp32('Button 2 pressed!')
+    return 'Button 2 pressed!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
