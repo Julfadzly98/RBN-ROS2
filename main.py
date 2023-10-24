448f954d7ce48da57513eda40b718ce082aec965
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 15:20:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import smbus
+import time
 
 # Create an I2C object
 bus = smbus.SMBus(1)  # 1 indicates /dev/i2c-1, for Raspberry Pi 2/3/4. Use 0 for Raspberry Pi 1.
 
 def send_to_esp32(message):
-    bus.write_i2c_block_data(0x8, 0, [ord(c) for c in message])  # 0x8 is the ESP32 I2C address
+    try:
+        # Convert message to a list of ASCII values
+        message_bytes = [ord(c) for c in message]
+        # Send data to the ESP32
+        bus.write_i2c_block_data(0x08, 0, message_bytes)  # 0x08 is the ESP32 I2C address
+        time.sleep(0.1)  # Add a small delay to allow ESP32 to process the data
+    except Exception as e:
+        print(f"Error: {e}")
 
 # Example usage:
 send_to_esp32('Hello from Raspberry Pi!')
