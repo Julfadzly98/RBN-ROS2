c34699544050c3af5d06ffc842309a10e8cf9ce1
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 17:13:21 +0800

    Update main.py

--- esp32/main.py
+++ esp32/main.py
@@ -1,67 +1,26 @@
-from machine import Pin
-from time import sleep
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+import requests
 
-# Define motor control pins
-motorA_input1 = Pin(5, Pin.OUT)  # Replace X with the GPIO pin number
-motorA_input2 = Pin(4, Pin.OUT)  # Replace Y with the GPIO pin number
-motorB_input1 = Pin(19, Pin.OUT)  # Replace Z with the GPIO pin number
-motorB_input2 = Pin(18, Pin.OUT)  # Replace W with the GPIO pin number
+app = Flask(__name__)
 
-def connect():
-    import network
- 
-    ssid = "smartspacekk" 
-    password = "smartspace09" 
- 
-    station = network.WLAN(network.STA_IF)
- 
-    if station.isconnected() == True:
-        print("Already connected")
-        return
- 
-    station.active(True)
-    station.connect(ssid, password)
- 
-    while station.isconnected() == False:
-        pass
- 
-    print("Connection successful")
-    print(station.ifconfig())
+# Set up GPIO on Raspberry Pi
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)  # Assuming you're using GPIO17 as output
 
-connect()
+# Define a function to send signal to ESP32
+def send_signal():
+    GPIO.output(17, GPIO.HIGH)  # Send HIGH signal
+    GPIO.output(17, GPIO.LOW)   # Reset to LOW
 
-def move_forward():
-    motorA_input1.on()
-    motorA_input2.off()
-    motorB_input1.on()
-    motorB_input2.off()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def move_backward():
-    motorA_input1.off()
-    motorA_input2.on()
-    motorB_input1.off()
-    motorB_input2.on()
+@app.route('/button_clicked', methods=['POST'])
+def button_clicked():
+    send_signal()
+    return "Signal sent to ESP32!"
 
-def stop_motors():
-    motorA_input1.off()
-    motorA_input2.off()
-    motorB_input1.off()
-    motorB_input2.off()
-
-def web_control(point):
-    if point == "point_a":
-        move_forward()
-    elif point == "point_b":
-        move_backward()
-    else:
-        stop_motors()
-
-while True:
-    data = input("Enter 'point_a' to move forward, 'point_b' to move backward, or 'stop' to stop motors: ")
-    if data == "point_a" or data == "point_b":
-        web_control(data)
-    elif data == "stop":
-        stop_motors()
-    else:
-        print("Invalid input. Please enter 'point_a', 'point_b', or 'stop'.")
-
+if __name__ == '__main__':
+    app.run(debug=True)
