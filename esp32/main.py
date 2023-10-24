686b15aca15b11da94d1116b736c3c5c7a71fdbd
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 19:36:12 +0800

    Update main.py

--- esp32/main.py
+++ esp32/main.py
@@ -1,10 +1,21 @@
 import machine
 
 # Define the GPIO pin connected to the Raspberry Pi
-pin = machine.Pin(16, machine.Pin.IN)
+pin = machine.Pin(2, machine.Pin.IN)
+pins = machine.Pin(4, machine.Pin.IN)
 
-def print_message(pin):
-    print("Signal received from Raspberry Pi!")
 
-# Attach an interrupt to the pin
-pin.irq(trigger=machine.Pin.IRQ_RISING, handler=print_message)
+while True:
+    
+    def print_message(pin):
+        print("Button 1 Received")
+
+    # Attach an interrupt to the pin
+    pin.irq(trigger=machine.Pin.IRQ_RISING, handler=print_message)
+    
+    def print_messages(pins):
+        print("Button 2 Received")
+
+    # Attach an interrupt to the pin
+    pins.irq(trigger=machine.Pin.IRQ_RISING, handler=print_messages)
+
