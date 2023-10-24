b8fc7ac050d950b20d3ad0114a0bd053a21b170a
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 17:39:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-import serial
-import time
-from flask import Flask, render_template
-
-ser = serial.Serial('/dev/serial0', 9600)  # Open the serial port
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
-def send_to_esp32(message):
-    try:
-        ser.write(message.encode())  # Send the message over serial
-        time.sleep(0.1)  # Add a small delay to allow ESP32 to process the data
-    except Exception as e:
-        print(f"Error: {e}")
+# Set up GPIO on Raspberry Pi
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)  # Assuming you're using GPIO17 as output for the first button
+GPIO.setup(18, GPIO.OUT)  # Assuming you're using GPIO18 as output for the second button
+
+# Define a function to send signal to ESP32
+def send_signal(pin):
+    GPIO.output(pin, GPIO.HIGH)  # Send HIGH signal
+    GPIO.output(pin, GPIO.LOW)   # Reset to LOW
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/button1')
-def button1():
-    send_to_esp32('Button 1 pressed!')
-    return 'Button 1 pressed!'
-
-@app.route('/button2')
-def button2():
-    send_to_esp32('Button 2 pressed!')
-    return 'Button 2 pressed!'
+@app.route('/button_clicked', methods=['POST'])
+def button_clicked():
+    button = request.form['button']
+    if button == 'button1':
+        send_signal(17)  # Sending signal for button 1
+    elif button == 'button2':
+        send_signal(18)  # Sending signal for button 2
+    return f"Signal sent to ESP32 for {button}"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
