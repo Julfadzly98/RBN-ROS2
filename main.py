b50cdb95b3e6c32be37b875918838ccf52f14048
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 15:49:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import serial
+import time
 
 ser = serial.Serial('/dev/serial0', 9600)  # Open the serial port
 
 def send_to_esp32(message):
     try:
         ser.write(message.encode())  # Send the message over serial
+        time.sleep(0.1)  # Add a small delay to allow ESP32 to process the data
     except Exception as e:
         print(f"Error: {e}")
 
