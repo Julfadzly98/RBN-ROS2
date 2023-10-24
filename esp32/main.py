a2fdc35ba2973da07cb13486d1d70f317a9fb091
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 17:41:24 +0800

    Update main.py

--- esp32/main.py
+++ esp32/main.py
@@ -1,1 +1,21 @@
+import machine
+
+# Define the GPIO pin connected to the Raspberry Pi for button 1
+pin_button1 = machine.Pin(16, machine.Pin.IN)
+
+# Define the GPIO pin connected to the Raspberry Pi for button 2
+pin_button2 = machine.Pin(17, machine.Pin.IN)
+
+
+while True:
+    def print_message(pin):
+        if pin == pin_button1:
+            print("Signal received from Button 1!")
+        elif pin == pin_button2:
+            print("Signal received from Button 2!")
+
+    # Attach interrupts to the pins
+    pin_button1.irq(trigger=machine.Pin.IRQ_RISING, handler=lambda pin: print_message(pin_button1))
+    pin_button2.irq(trigger=machine.Pin.IRQ_RISING, handler=lambda pin: print_message(pin_button2))
+
 
