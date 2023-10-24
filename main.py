48329fb97232e3be5f699112360eb230ea7e94eb
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-24 19:43:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ app = Flask(__name__)
 
 # Set up GPIO on Raspberry Pi
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)  # Assuming you're using GPIO17 as output
+GPIO.setup(14, GPIO.OUT)  # Assuming you're using GPIO17 as output
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(15, GPIO.OUT)
 
 # Define a function to send signal to ESP32
 def send_signal():
-    GPIO.output(17, GPIO.HIGH)  # Send HIGH signal
-    GPIO.output(17, GPIO.LOW)   # Reset to LOW
+    GPIO.output(14, GPIO.HIGH)  # Send HIGH signal
+    GPIO.output(14, GPIO.LOW)   # Reset to LOW
+
+def send_signals():
+    GPIO.output(15, GPIO.HIGH)  # Send HIGH signal
+    GPIO.output(15, GPIO.LOW)   # Reset to LOW
 
 @app.route('/')
 def index():
@@ -20,7 +26,12 @@ def index():
 @app.route('/button_clicked', methods=['POST'])
 def button_clicked():
     send_signal()
-    return "Signal sent to ESP32!"
+    return "Button 1"
+
+@app.route('/button_clickeds', methods=['POST'])
+def button_clickeds():
+    send_signals()
+    return "Button 2"
 
 if __name__ == '__main__':
     app.run(debug=True)
